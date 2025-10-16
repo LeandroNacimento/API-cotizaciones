c7b34ef81d75e7cb6769de60af94ceee96d86b40
Leandro Nacimento <leandronacimento04@gmail.com>
2025-10-16 20:01:39 -0300

    chore(deps): actualización de dependencias; agregar base layout (vite+livewire) e integración Tailwind v4

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es-AR" class="h-full" style="color-scheme: light dark;">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ isset($title) ? $title : 'App' }}</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    @livewireStyles
+</head>
+<body class="h-full bg-gray-50 dark:bg-gray-900">
+    <main>
+        {{ $slot }}
+    </main>
+    @livewireScripts
+    <script defer src="//unpkg.com/alpinejs"></script>
+</body>
+</html>
